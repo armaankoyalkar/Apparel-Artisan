1b2ef92ef9da91e4e060c642e498bd8815e7e3f7
armaan koyalkar <armaankoyalkar@gmail.com>
2026-06-07 21:18:19 +0530

    Refactor MongoDB connection

--- ecommerce-backend/config/db.js
+++ ecommerce-backend/config/db.js
@@ -3,18 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      // Options to avoid deprecation warnings
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // useCreateIndex is deprecated, use index: true in schema definition instead
-      // useFindAndModify: false is deprecated, use findOneAndUpdate/deleteOne etc. directly
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
